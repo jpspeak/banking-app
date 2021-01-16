08cc5ee789a3ae1331cd68ac7ee0491fe6099bb4
immanuelMM <merino.mahusay@gmail.com>
2021-01-16 14:13:16 +0800

    fix the bug for add user

--- immanuel.js
+++ immanuel.js
@@ -1,44 +1,54 @@
-let user_account = [
+var user_account = [
     {
-        name: "jay",
+        name: "JAY",
         balance: 0,
     },
     {
-        name: "Imman",
+        name: "IMMAN",
         balance: 100,
     },
 ]
 
 function new_user(user, balance){
 
+    var user = user.toUpperCase()
+
     let obj = {name:user, balance:balance}; 
 
-    for (i = 0; i < user_account.length ; i++){
-   
-        var arruser  = user_account[i];
-   
-    }
+    for (var i = 0; i < user_account.length ; i++){
 
-
-    if( arruser.name == user.toUpperCase() || arruser.name == user ){
+        var user_arr = user_account[i];
       
+    }
+
+    if( user_account[0].name == user){  
    
+        console.log("the user your enter it's already registered " + user.toUpperCase());    
+   
+    }else if(user_account[1].name == user){
+
         console.log("the user your enter it's already registered " + user.toUpperCase());
-        
-   
-    }
-     else{
-   
-        user_account.push(obj.toUpperCase());
+
+    }else if(user_arr.name === user){
+
+        console.log("the user your enter it's already registered " + user.toUpperCase());
+
+    }else{
+
+        user_account.push(obj);
     
-        console.log("Successfully added new user " + user);
+        // console.log("Successfully added new user " + user);
+
+        return "Successfully added new user " + user;
    
-    }
+        }
 
 }
 
 function deposite(user, amount){
 
+    var user = user.toUpperCase();
+    
     for (i = 0; i < user_account.length ; i++){
         
         // var arr_user = user_account[i];
@@ -48,17 +58,20 @@ function deposite(user, amount){
 
             let total =  user_account[i].balance = dep
             
-            console.log("deposite")
+            // console.log("deposite")
       
-            console.log("Your new balance is " + total)
+            // console.log("Your new balance is " + total)
       
+            return "Your new balance is " + total
         }
     }
 
 }
 
 function withdraw(user, amount){
-  
+    
+    var user = user.toUpperCase();
+
     for (i = 0; i < user_account.length ; i++){
         
         // var arr_user = user_account[i];
@@ -68,9 +81,11 @@ function withdraw(user, amount){
 
             let total =  user_account[i].balance = dep
        
-            console.log("withdraw")
+            // console.log("withdraw")
        
-            console.log("Your new balance is " + total)
+            // console.log("Your new balance is " + total)
+
+            return "Your new balance is " + total;
        
         }
     }
